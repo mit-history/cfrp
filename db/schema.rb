455e59b2bb22e3b13c80b511a66bf24771ea0103
Dave Della Costa <dfd@mit.edu>
2012-04-30 22:37:39 +0900

    Changes to Devise-related user fields.

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110614135453) do
+ActiveRecord::Schema.define(:version => 20120430110539) do
 
   create_table "comment_types", :force => true do |t|
     t.string   "name"
@@ -148,7 +149,6 @@ ActiveRecord::Schema.define(:version => 20110614135453) do
     t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
     t.string   "password_salt",                       :default => "", :null => false
     t.string   "reset_password_token"
-    t.string   "remember_token"
     t.datetime "remember_created_at"
     t.integer  "sign_in_count",                       :default => 0
     t.datetime "current_sign_in_at"
