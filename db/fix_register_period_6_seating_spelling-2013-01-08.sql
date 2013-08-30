504d8e09ca434e0489866aad9f79159f4c73b2f5
Jamie Folsom <jamiefolsom@gmail.com>
2013-08-30 16:04:11 -0400

    Coupla tweaks.

--- db/fix_register_period_6_seating_spelling-2013-01-08.sql
+++
@@ -1,4 +0,0 @@
-UPDATE seating_categories SET name = 'Troisièmes Loges à 6 places', description = 'Troisièmes Loges à 6 places' WHERE id = 31;
-UPDATE seating_categories SET name = 'Troisièmes Loges à 4 places', description = 'Troisièmes Loges à 4 places' WHERE id = 32;
-UPDATE seating_categories SET name = 'Premieres Places à 6 livres', description = 'Premieres Places à 6 livres' WHERE id = 37;
-UPDATE seating_categories SET name = 'Troisièmes Places à 2 livres', description = 'Troisièmes Places à 2 livres' WHERE id = 40;
