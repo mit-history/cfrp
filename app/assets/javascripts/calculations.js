389c14d5b2693c6a53be08ef8e08fabdbfef7b95
Jamie Folsom <jamiefolsom@gmail.com>
2014-02-17 21:16:39 -0500

    WIP changes to the calculation script to support deniers.

--- app/assets/javascripts/calculations.js
+++ app/assets/javascripts/calculations.js
@@ -4,11 +4,15 @@
  * These depend on the specific selectors in that template, and jquery...this isn't very portable...
  *  -DD, 12/09/09
  * 
+ * Updated to accommodate deniers, a base-12 sub-sous denomination, in addition to livres and sous.
+ *  -JF, 02/17/14
+ *  Wow this project has been around for a while!
  */
 
 var calculate_totals_for_tickets_sold = function () {
   var total_sold_l = 0;
   var total_sold_s = 0;
+  var total_sold_d = 0;
 
   $("table#ticket_sales_fields").find("input.ticket_sales_total_sold").each(
     function (i) {
@@ -19,23 +23,30 @@ var calculate_totals_for_tickets_sold = function () {
 
       var ppt_l      = 0;
       var ppt_s      = 0;
+      var ppt_d      = 0;
 
       ppt_s = $("table#ticket_sales_fields").find("input#register_ticket_sales_attributes_" + this_index + "_price_per_ticket_s").attr('value');
       ppt_l = $("table#ticket_sales_fields").find("input#register_ticket_sales_attributes_" + this_index + "_price_per_ticket_l").attr('value');
+      ppt_d = $("table#ticket_sales_fields").find("input#register_ticket_sales_attributes_" + this_index + "_price_per_ticket_d").attr('value');
 
-      this_total_l = (ppt_l * total_sold) + Math.floor((ppt_s * total_sold)/20);
-      this_total_s = (ppt_s * total_sold) % 20;
+      // this_total_l = (ppt_l * total_sold) + Math.floor((ppt_s * total_sold)/20);
+      this_total_l = (ppt_l * total_sold) + plus_ça_change(ppt_s, ppt_d, total_sold);
+      // this_total_s = ((ppt_s * total_sold) % 20) + Math.floor((ppt_d * total_sold)/12);
+      this_total_s = ((ppt_s * total_sold) % 20) + Math.floor((ppt_d * total_sold)/12);
+      // this_total_d = (ppt_d * total_sold) % 12;
+      this_total_d = (ppt_d * total_sold) % 12;
 
       total_sold_l += this_total_l;
       total_sold_s += this_total_s;
+      total_sold_d += this_total_d;
 
       // One more time to make sure we haven't 'gone over' with sous:
-      total_sold_l = (total_sold_l + Math.floor(this_total_s / 20));
+      total_sold_l = (total_sold_l + Math.floor(this_total_s / 20) + Math.floor(this_total_d / 12));
       // total_sold_s = (this_total_s - this_total_s % 20);
     }
   );
 
-  if (isNaN(total_sold_l) || isNaN(total_sold_s)) {
+  if (isNaN(total_sold_l) || isNaN(total_sold_s) || isNaN(total_sold_d)) {
     $('#total_receipts_calculated_ts').html('Not a number--please review your<br />"Tickets Sold and Price Per Ticket"<br />fields for non-number values');
     $('#total_receipts_calculated_ts').css('font-size', '11px');
   } else {
@@ -43,15 +54,21 @@ var calculate_totals_for_tickets_sold = function () {
     // sous to add up to a livre, but we don't display it properly.
     total_sold_l = total_sold_l + Math.floor(total_sold_s/20);
     total_sold_s = total_sold_s % 20;
+    total_sold_d = total_sold_d % 12;
     $('#total_receipts_calculated_ts').html("Tally: " + total_sold_l + " livre, " + total_sold_s + " sous");
     $('#total_receipts_calculated_ts').css('font-size', '14px');
   }
 
   compare_to_calculated_totals(total_sold_l, total_sold_s, 'total_receipts_calculated_ts');
 
-  return { l: total_sold_l, s: total_sold_s };
+  return { l: total_sold_l, s: total_sold_s, d: total_sold_d };
 };
 
+// function plus_ça_change(ppt_s, ppt_d, total_sold) {
+//   var livres = 0;
+//   Math.floor((ppt_d * total_sold)/12) 
+//   return 
+// }
 
 var calculate_totals_for_recorded_take = function () {
   var total_l = 0;
