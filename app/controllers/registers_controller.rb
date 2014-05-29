f78c379ef815d2eb0b3d5356b0bd2381083bea23
Jamie Folsom <jamiefolsom@gmail.com>
2014-05-29 17:09:13 -0400

    Faceting demo ready for review by Agathe.

--- app/controllers/registers_controller.rb
+++ app/controllers/registers_controller.rb
@@ -1,6 +1,8 @@
 class RegistersController < ApplicationController
   include Repertoire::Faceting::Controller
 
+  before_filter :authenticate_user!, :only => [:new, :edit, :create, :update, :destroy]
+
   def index
     @search = params[:search] || ''
   end
@@ -35,6 +37,23 @@ class RegistersController < ApplicationController
     redirect_to :action => "index", :notice => 'Register was successfully updated.'
   end
 
+  def results
+    filter = params[:filter] || {}
+    limit  = params[:limit]  || false
+    offset = params[:offset] || false
+
+    @total = base.refine(filter)
+    @refined = @total.limit(limit) if limit
+    @refined = @refined.offset(offset) if offset
+    @results = @refined.to_a
+
+    @count = @total.count
+
+    respond_to do |format|
+      format.html { render @results, :layout => false }
+      format.json { render :json => @results }
+    end
+  end
 
 
   protected
