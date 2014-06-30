6e3c3715df79e4b594f5d0ac4e756bfe11f4bab7
Jamie Folsom <jamiefolsom@gmail.com>
2014-06-30 11:37:43 -0400

    Allow admins to view and edit registers through the admin UI.

--- app/admin/registers.rb
+++ app/admin/registers.rb
@@ -98,9 +98,9 @@ ActiveAdmin.register Register do
 
     column "" do |resource|
       links = ''.html_safe
+      links += link_to I18n.t('active_admin.view'), resource_path(resource), :class => "member_link view_link"
+      links += link_to I18n.t('active_admin.edit'), edit_resource_path(resource), :class => "member_link edit_link"
       if current_user.has_role?(:super_admin)
-        links += link_to I18n.t('active_admin.view'), resource_path(resource), :class => "member_link view_link"
-        links += link_to I18n.t('active_admin.edit'), edit_resource_path(resource), :class => "member_link edit_link"
         links += link_to I18n.t('active_admin.delete'), resource_path(resource), :method => :delete, :confirm => I18n.t('active_admin.delete_confirmation'), :class => "member_link delete_link"
       end
       links += link_to("Formulaire de saisie", "/registers/#{resource.id}/edit", target: "_blank")
