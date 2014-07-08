82222bf4eb9817816057345c0251a66c249e28bb
Christopher W York <christopher.w.york@gmail.com>
2014-07-08 11:42:53 +0100

    Accommodate Heroku's memory requirements in the data-essays JSON feed.

--- app/controllers/play_performances_controller.rb
+++ app/controllers/play_performances_controller.rb
@@ -1,12 +1,16 @@
 class PlayPerformancesController < ActionController::Base
-  
+
   def index
     filter = params[:filter] || {}
-    limit  = params[:limit]  || false
-    offset = params[:offset] || false
 
     if stale?(Register.facet_cache_key)
-      render :json => Register.refine(filter).joins(:register_plays).offset(offset).limit(limit)
+      # (1) Rails JSON rendering is quite slow
+      # (2) Heroku memory requirements forbid loading Register.all with ActiveRecord
+      # Combined solution: retrieve shell objects with only necessary columns
+      render :json => Register.refine(filter).select([:id, :date])
+      # (another option: render ..., :only => [:id, :date])
+      # See the example data-essays page for alternatives to serve large JSON feeds.
     end
   end
+
 end
