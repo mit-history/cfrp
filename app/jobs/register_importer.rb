af0b85c391facc5b529c9d0a37549ab3cf16dc47
Jamie Folsom <jamiefolsom@gmail.com>
2015-08-29 08:10:47 -0400

    Update image migration tasks for R134-R158

--- app/jobs/register_importer.rb
+++ app/jobs/register_importer.rb
@@ -9,18 +9,19 @@ class RegisterImporter
     register = Register.find(@register_id)
     # if there's an orientation on the first image, it's been processed; skip it.
     print "\n\n============\n #{register.id} \n"
-    #  next if (!register.register_images[0].orientation.nil?)
+    
+    next if (!register.register_images[0].orientation.nil?)
 
     puts "...not processed yet, processing..."
     recto = register.recto_image
-    verso = register.verso_image
-    left = register.left_image
+    # verso = register.verso_image
+    # left = register.left_image
 
-    if verso.nil?
-      print "Verso was nil for register: #{register.id} \n"
-    else
-      save_register_image(verso, 'verso')
-    end
+    # if verso.nil?
+    #   print "Verso was nil for register: #{register.id} \n"
+    # else
+    #   save_register_image(verso, 'verso')
+    # end
 
     if recto.nil?
       print "Recto was nil for register: #{register.id} \n"
@@ -28,53 +29,53 @@ class RegisterImporter
       save_register_image(recto, 'recto')
     end
 
-    if left.nil?
-      print "Left was nil for register: #{register.id} \n"
-    else
-      create_register_image(register, left, 'left')
-    end
+    # if left.nil?
+    #   print "Left was nil for register: #{register.id} \n"
+    # else
+    #   create_register_image(register, left, 'left')
+    # end
   end
 
   private
 
   def save_register_image(register_image, orientation)
     begin
-      # print "\n******* \n Existing\n"
-      # print "Register: #{register_image.register_id} \n"
-      # print "Register image: #{register_image.id} \n"
-      # print "Register image filepath: #{register_image.filepath} \n"
-      # print "Requested orientation: #{orientation} \n"
-      # print "Register image rv flag: #{register_image.rv_flag} \n"
-
-      register_image.orientation = orientation
-      register_image.image = "http://images.cfregisters.org/#{register_image.image_filename}"
-      # print "Register image: #{register_image.inspect} \n"
-      register_image.save!
-    rescue StandardError => e
-      puts e.inspect
-    end
-  end
-
-  def create_register_image(register, register_image, orientation)
-    begin
-      print "\n++++++ \n New\n"
-      print "Previous Register: #{register.id} \n"
-      print "Old register image: #{register_image.id} \n"
+      print "\n******* \n Existing\n"
+      print "Register: #{register_image.register_id} \n"
+      print "Register image: #{register_image.id} \n"
+      print "Register image filepath: #{register_image.filepath} \n"
       print "Requested orientation: #{orientation} \n"
       print "Register image rv flag: #{register_image.rv_flag} \n"
 
-      new_image = RegisterImage.new(
-        orientation: orientation,
-        register_id: register.id,
-        # image: "http://images.cfregisters.org/#{register_image.image_filename}",
-        image: "http://images.cfregisters.org/#{register.left_image.image_filename}",
-        image_content_type: "image/jpeg"
-      )
-
-      print "New register image: #{new_image.inspect} \n"
-      new_image.save!
+      register_image.orientation = orientation
+      register_image.image = "https://s3.amazonaws.com/seventies-nineties/#{register_image.image_filename}"
+      print "Register image: #{register_image.inspect} \n"
+      # register_image.save!
     rescue StandardError => e
       puts e.inspect
     end
   end
+
+  # def create_register_image(register, register_image, orientation)
+  #   begin
+  #     print "\n++++++ \n New\n"
+  #     print "Previous Register: #{register.id} \n"
+  #     print "Old register image: #{register_image.id} \n"
+  #     print "Requested orientation: #{orientation} \n"
+  #     print "Register image rv flag: #{register_image.rv_flag} \n"
+
+  #     new_image = RegisterImage.new(
+  #       orientation: orientation,
+  #       register_id: register.id,
+  #       # image: "http://images.cfregisters.org/#{register_image.image_filename}",
+  #       image: "http://images.cfregisters.org/#{register.left_image.image_filename}",
+  #       image_content_type: "image/jpeg"
+  #     )
+
+  #     print "New register image: #{new_image.inspect} \n"
+  #     new_image.save!
+  #   rescue StandardError => e
+  #     puts e.inspect
+  #   end
+  # end
 end
--- lib/tasks/image_management.rake
+++ lib/tasks/image_management.rake
@@ -1,12 +1,17 @@
 namespace :filepath do
   desc 'enqueue Registers for migration'
-  task migrate_all_two_image_registers_to_paperclip: :environment do
-    Register.two_images.find_each do |register|
-      Delayed::Job.enqueue RegisterImporter.new(register.id)
-    end
-  end
-  task add_left_to_1758_1759_registers: :environment do
-    Register.season('1758-1759').unentered.find_each do |register|
+  # task migrate_all_two_image_registers_to_paperclip: :environment do
+  #   Register.two_images.find_each do |register|
+  #     Delayed::Job.enqueue RegisterImporter.new(register.id)
+  #   end
+  # end
+  # task add_left_to_1758_1759_registers: :environment do
+  #   Register.season('1758-1759').unentered.find_each do |register|
+  #     Delayed::Job.enqueue RegisterImporter.new(register.id)
+  #   end
+  # end
+  task migrate_seventies_nineties: :environment do
+    Register.volume_number.between?(134,158).find_each do |register|
       Delayed::Job.enqueue RegisterImporter.new(register.id)
     end
   end
