bc09c143fac95eeea611b08fde5bea0f069f1004
Christopher W York <christopher.w.york@gmail.com>
2015-08-29 13:45:49 +0200

    Fix Ravel seating category profile (reverse Odeon & Tuileries)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150817163448) do
+ActiveRecord::Schema.define(:version => 20150829113121) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
