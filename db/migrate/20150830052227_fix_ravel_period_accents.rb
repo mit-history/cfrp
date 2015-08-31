0ff70b6d9d93f503b99310d13fee241d069a5ea2
Christopher W York <christopher.w.york@gmail.com>
2015-08-31 07:57:50 +0200

    Fix Ravel seating category profile 2 (accents)

---
+++ db/migrate/20150830052227_fix_ravel_period_accents.rb
@@ -0,0 +1,33 @@
+class FixRavelPeriodAccents < ActiveRecord::Migration
+
+  # seating category profiles table has been updated in git; drop old records and reload it
+  def up
+
+    dir = File.dirname(__FILE__)
+
+    execute "DELETE FROM seating_category_profile WHERE profile='Ravel 1'"
+
+    dbconn = ActiveRecord::Base.connection_pool.checkout
+    raw  = dbconn.raw_connection
+    count = nil
+
+    result = raw.copy_data %q{COPY seating_category_profile(profile, period, start_date, end_date, seating_category_ids, category, note) FROM STDIN WITH (FORMAT csv)} do
+      File.open(dir + '/../data/2015.06.25.ravel_seating_category_profile.csv', 'r').each do |line|
+        raw.put_copy_data line
+      end
+    end
+
+    count = dbconn.select_value("select count(*) from seating_category_profile").to_i
+
+    ActiveRecord::Base.connection_pool.checkin(dbconn)
+
+    count
+
+  end
+
+  def down
+
+    execute "DELETE FROM seating_category_profile WHERE profile='Ravel 1'"
+
+  end
+end
